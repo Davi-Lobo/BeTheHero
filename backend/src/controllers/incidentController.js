d9c35c5ef842ec7f15f9b57f883d83501138cbbf
Davi Lobo <davi.lobo.gs@gmail.com>
2020-03-26 20:04:29 -0300

    Delete route created

---
+++ backend/src/controllers/incidentController.js
@@ -0,0 +1,42 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async index(req, res) {
+        const incidents = await connection('incidents').select('*');
+        
+        return res.json(incidents);
+    },
+
+    async create(req, res) {
+        const { title, description, value } = req.body;
+
+        const ong_id = req.headers.authorization;
+
+        const [id] = await connection('incidents').insert({
+            title,
+            description,
+            value,
+            ong_id,
+        });
+
+        return res.json({ id });
+    },
+
+    async delete(req, res) {
+        const { id } = req.params;  
+        const ong_id = req.headers.authorization;
+
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first();
+
+        if (incident.ong_id != ong_id) {
+            return res.status(401).json({error: 'Operation unauthorized'});
+        }
+
+        await connection('incidents').where('id', id).delete();
+        
+        return res.status(204).send();
+    }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const ongController = require('./controllers/ongController');
+const incidentController = require('./controllers/incidentController');
 const connection = require('./database/connection');
 
 const routes = express.Router();
@@ -8,5 +9,9 @@ const routes = express.Router();
 routes.get('/ongs', ongController.index);
 
 routes.post('/ongs', ongController.create);
+
+routes.get('/incidents', incidentController.index);
+routes.post('/incidents', incidentController.create);
+routes.delete('/incidents/:id', incidentController.delete)
 
 module.exports = routes;
