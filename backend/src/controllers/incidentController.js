b9d5fef724e1665cea8f5b2b72163f75d9d2a925
Davi Lobo <davi.lobo.gs@gmail.com>
2020-03-26 20:26:00 -0300

    Incidents pagination

--- backend/src/controllers/incidentController.js
+++ backend/src/controllers/incidentController.js
@@ -2,7 +2,18 @@ const connection = require('../database/connection');
 
 module.exports = {
     async index(req, res) {
-        const incidents = await connection('incidents').select('*');
+        const { page = 1 } = req.query;
+
+        const [count] = await connection('incidents').count();
+
+        console.log(count)
+
+        const incidents = await connection('incidents')
+            .limit(6)
+            .offset((page-1) * 6)
+            .select('*');
+
+        res.header('X-Total-Count', count['count(*)']);
         
         return res.json(incidents);
     },
