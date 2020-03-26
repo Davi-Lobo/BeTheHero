96ac0f5621ba512a44a5bdff4b350af15f241812
Davi Lobo <davi.lobo.gs@gmail.com>
2020-03-26 19:10:17 -0300

    New folder structure and knex sqlite migrations created

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,10 @@
  const express = require('express');
 
+ const routes = require('./routes');
+
  const app = express();
 
- app.get('/', (req, res) => {
-     return res.send('Hello World')
- })
- 
+ app.use(express.json());
+ app.use(routes);
 
  app.listen(3333);
