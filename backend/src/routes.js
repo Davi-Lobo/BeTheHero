85933894df00c3f822405ae0d002f35956e8e9e6
Davi Lobo <davi.lobo.gs@gmail.com>
2020-03-26 19:37:11 -0300

    Creating DB connection and GET route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const crypto = require('crypto');
+
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.send('Hello World');
+routes.get('/ongs', async (req, res) => {
+    const ongs = await connection('ongs').select('*');
+
+    return res.json(ongs);
+});
+
+routes.post('/ongs', async (req, res) => {
+    const { name, email, whatsapp, city, uf } = req.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
+    });
+
+    return res.json({ id });
 });
 
 module.exports = routes;
