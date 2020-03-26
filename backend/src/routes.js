7985124fc6d2ad409f0bac315beef26385244232
Davi Lobo <davi.lobo.gs@gmail.com>
2020-03-26 20:12:41 -0300

    Route for specific list of cases

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 
 const ongController = require('./controllers/ongController');
+const profileController = require('./controllers/profileController');
 const incidentController = require('./controllers/incidentController');
 const connection = require('./database/connection');
 
 const routes = express.Router();
 
 routes.get('/ongs', ongController.index);
-
 routes.post('/ongs', ongController.create);
+
+routes.get('/profile', profileController.index);
 
 routes.get('/incidents', incidentController.index);
 routes.post('/incidents', incidentController.create);
