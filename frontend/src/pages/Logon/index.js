6f142615c02ea7241104589eac135998f0ccdc75
Davi Lobo <davi.lobo.gs@gmail.com>
2020-03-27 03:08:51 -0300

    Changing tag A to Link component

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './style.css';
@@ -19,10 +20,10 @@ export default function Logon() {
                     <input type="text" placeholder="Sua ID"/>
                     <button className="button-submit" type="submit">Entrar</button>
                     
-                    <a href="/register">
+                    <Link to="/register">
                         <FiLogIn size={16} color="E02041"/>
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
 
