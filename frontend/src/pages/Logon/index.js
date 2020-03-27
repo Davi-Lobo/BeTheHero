1826074c268563e9856b2c89b740bab0d4ddc91a
Davi Lobo <davi.lobo.gs@gmail.com>
2020-03-27 03:23:05 -0300

    Register page structure and style

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
-import './style.css';
-
-
 import logo from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
+
+import './style.css';
 
 export default function Logon() {
     return (
         <div className="logon-container">
             <section className="form">
-                <img src={logo} alt="Logo" className="img"/>
+                <img src={logo} alt="Be The Hero" className="img"/>
 
                 <form action="">
                     <h1>Faça seu logon</h1>
@@ -20,7 +19,7 @@ export default function Logon() {
                     <input type="text" placeholder="Sua ID"/>
                     <button className="button-submit" type="submit">Entrar</button>
                     
-                    <Link to="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="E02041"/>
                         Não tenho cadastro
                     </Link>
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
 
 import './style.css';
 
 export default function Register() {
-    return <h1>Register!</h1>
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logo} alt="Be The Hero" className="img"/>
+
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+
+                    <Link className="back-link" to="/register">
+                        <FiArrowLeft size={16} color="E02041"/>
+                        Voltar para o logon
+                    </Link>
+
+                </section>
+
+                <form action="">
+                    <input type="text" placeholder="Nome da ONG"/>
+                    <input type="email" placeholder="E-mail"/>
+                    <input type="text" placeholder="Whatsapp"/>
+
+                    <div className="input-group">
+                        <input type="text" placeholder="Cidade"/>
+                        <input type="text" placeholder="UF" style={{ width: 80 }}/>
+                    </div>
+
+                    <button className="button-submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    )
 }
