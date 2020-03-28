4129a49b0a44ee7164d68b8e09b0037fea931f5d
Davi Lobo <davi.lobo.gs@gmail.com>
2020-03-28 02:30:27 -0300

    New incidents now working

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
 import './style.css';
 
 export default function NewIncident() {
+    const history = useHistory();
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+
+    const ongId = localStorage.getItem('ongId');
+
+    async function handleNewIncident(e) {
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value,
+        };
+
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId,
+                }
+            });
+
+            history.push('/profile');
+        } catch(err) {
+            alert('Erro ao cadastrar caso, tente novamente');
+        }
+    }
+
     return (
         <div className="new-incident-container">
             <div className="content">
@@ -16,17 +48,34 @@ export default function NewIncident() {
                     <h1>Cadastrar novo caso</h1>
                     <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
 
-                    <Link className="back-link" to="/">
+                    <Link className="back-link" to="/profile">
                         <FiArrowLeft size={16} color="E02041"/>
                         Voltar para home
                     </Link>
 
                 </section>
 
-                <form action="">
-                    <input type="text" placeholder="Titulo do caso"/>
-                    <textarea type="email" placeholder="Descrição"/>
-                    <input type="text" placeholder="Valor em reais"/>
+                <form onSubmit={handleNewIncident}>
+                    <input 
+                        type="text" 
+                        placeholder="Titulo do caso"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+
+                    <textarea 
+                        type="email" 
+                        placeholder="Descrição"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+
+                    <input 
+                        type="number" 
+                        placeholder="Valor em reais"
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
 
                     <button className="button-submit">Cadastrar</button>
                 </form>
